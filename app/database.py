9e1d046eac30c8cdd260b76ee3ba6b41375ea16d
robin-vidal <robin.vidal.pro@gmail.com>
2026-06-16 10:56:03 +0200

    fix: lazy load db engine to avoid SSM call at import time

--- app/database.py
+++ app/database.py
@@ -1,14 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from app.config import get_db_url
 
-engine = create_engine(get_db_url())
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+_engine = None
+_SessionLocal = None
+
+
+def get_engine():
+    global _engine
+    if _engine is None:
+        from app.config import get_db_url
+        _engine = create_engine(get_db_url())
+    return _engine
+
+
+def get_session_local():
+    global _SessionLocal
+    if _SessionLocal is None:
+        _SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
+    return _SessionLocal
 
 
 def get_db():
-    db = SessionLocal()
+    db = get_session_local()()
     try:
         yield db
     finally:

118c395a98d99c5f29349e6d99332e653734d9a7
robin-vidal <robin.vidal.pro@gmail.com>
2026-06-16 10:46:46 +0200

    feat: add database connection

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from app.config import get_db_url
+
+engine = create_engine(get_db_url())
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
